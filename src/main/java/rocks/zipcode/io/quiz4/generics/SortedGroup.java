406310572f911452eb4eab5cfb165e069ae9aafe
Sean MacLaughlin <seanmaclaughlin@zipcoders-MacBook-Pro.local>
2019-04-08 20:44:58 -0400

    Monday night Checkpoint

--- src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
@@ -2,42 +2,34 @@ package rocks.zipcode.io.quiz4.generics;
 
 import javafx.collections.transformation.SortedList;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author leon on 18/12/2018.
  */
-public class SortedGroup<_> extends Group<_> implements Comparable<_>{
+public class SortedGroup<_ extends Comparable> extends Group<_>{
     private List<_> myList = new ArrayList<>();
 
+
     public SortedGroup() {
-    }
-
-
-    public List<_> sorter(List myList) {
-    Collections.sort(myList);
-    return myList;
     }
 
     @Override
     public void insert(_ value) {
         myList.add(value);
+        myList.sort(Comparator.naturalOrder());
     }
 
     @Override
     public void delete(_ value) {
         myList.remove(value);
+        myList.sort(Comparator.naturalOrder());
     }
 
     public Integer indexOf(_ value) {
+        myList.sort(Comparator.naturalOrder());
         return myList.indexOf(value);
+
     }
 
-    @Override
-    public int compareTo(_ o) {
-        return 0;
-    }
 }
